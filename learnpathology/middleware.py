3fe06a0d1144010957320207a49619f9225bdf4f
Erik Smistad <ersmistad@gmail.com>
2021-09-06 15:35:29 +0200

    Added login required middleware

---
+++ learnpathology/middleware.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from re import compile
+
+EXEMPT_URLS = [compile(settings.LOGIN_URL.lstrip('/'))]
+if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
+    EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
+
+from django.utils.deprecation import MiddlewareMixin
+
+
+class LoginRequiredMiddleware(MiddlewareMixin):
+    """
+    Middleware that requires a user to be authenticated to view any page other
+    than LOGIN_URL. Exemptions to this requirement can optionally be specified
+    in settings via a list of regular expressions in LOGIN_EXEMPT_URLS (which
+    you can copy from your urls.py).
+    """
+    def process_request(self, request):
+        assert hasattr(request, 'user'), """
+        The Login Required middleware needs to be after AuthenticationMiddleware.
+        Also make sure to include the template context_processor:
+        'django.contrib.auth.context_processors.auth'."""
+        if not request.user.is_authenticated:
+            path = request.path_info.lstrip('/')
+            if not any(m.match(path) for m in EXEMPT_URLS):
+                return HttpResponseRedirect(settings.LOGIN_URL)
--- learnpathology/settings.py
+++ learnpathology/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'learnpathology.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'learnpathology.urls'
