fd95909bcc5aeb42ec20b2adf8b45ee8980f6e05
ingridtv <ingridtveten@hotmail.com>
2021-10-15 13:05:47 +0200

    Added listener to button classes rather than id to fix bug
    Also added disabling/enabling a button until the ajax request is handled

--- course/static/course/button.js
+++ course/static/course/button.js
@@ -1,46 +1,56 @@
 $(document).ready(function() {
 
-    $('#add_button').click(function () {
+    $('.add_button').on('click', function() {
 
-        var model_name = $(this).attr('model_name')
-        var instance_id = $(this).attr('instance_id')
-        console.log("Add button for " + model_name + " with id " + instance_id + " was clicked")
+        $(this).attr("disabled", true); // disable buttons
+
+        var course_id = $(this).data('course_id');
+        var model_name = $(this).data('model_name');
+        var instance_id = $(this).data('instance_id');
+        console.log("Add button for " + model_name + " with id " + instance_id + " clicked");
 
         $.ajax({
             url: "/course/add_item",
             type: "GET",
             data: {
-                'course_id': $(this).attr('course_id'),
+                'course_id': course_id,
                 'model_name': model_name,
                 'instance_id': instance_id,
             },
             success: function (xml) {
-                location.reload()  // Using .reload() method
+                $(this).attr("disabled", false); // re-enable upon success
+                location.reload();  // Using .reload() method
             },
-            error: function(result) {
+            error: function (result) {
                 alert('Error occurred when adding item to course');
+                $(this).attr("disabled", false); // re-enable even if not successful
             }
         });
     });
 
-    $('#remove_button').click(function () {
+    $('.remove_button').on('click',function () {
 
-        var model_name = $(this).attr('model_name')
-        var instance_id = $(this).attr('instance_id')
-        console.log("Remove button for " + model_name + " with id " + instance_id + " was clicked")
+        $(this).attr("disabled", true); // disable buttons
+
+        var course_id = $(this).data('course_id');
+        var model_name = $(this).data('model_name');
+        var instance_id = $(this).data('instance_id');
+        console.log("Remove button for " + model_name + " with id " + instance_id + " was clicked");
 
         $.ajax({
             url: "/course/remove_item",
             type: "GET",
             data: {
-                'course_id': $(this).attr('course_id'),
+                'course_id': course_id,
                 'model_name': model_name,
                 'instance_id': instance_id,
             },
             success: function (xml) {
-                location.reload()  // Using .reload() method
+                $(this).attr("disabled", false); // re-enable upon success
+                location.reload();  // Using .reload() method
             },
             error: function(result) {
+                $(this).attr("disabled", false); // re-enable even if not successful
                 alert('Error occurred when removing item from course');
             }
         });
