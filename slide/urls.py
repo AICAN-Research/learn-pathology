79275ad56a8d54fccf1738f098561fef74b48524
ingridtv <ingridtveten@hotmail.com>
2022-12-09 14:07:09 +0100

    Rename

--- slide/urls.py
+++ slide/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path('edit-tags/<int:slide_id>', views.edit_general_pathology_tags, name='edit_tags'),
     path('add-tag/', views.add_tag, name='add_tag'),
     path('remove-tag/', views.remove_tag, name='remove_tag'),
-    path('base-annotations/<int:slide_id>/', views.add_or_edit_descriptive_annotation, name='add_annotations'),
+    path('base-annotations/<int:slide_id>/', views.add_or_edit_descriptive_annotation, name='base_annotations'),
 ] + static('thumbnails/', document_root=os.path.join(settings.BASE_DIR, 'thumbnails'))
 
