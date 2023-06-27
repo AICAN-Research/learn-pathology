39dd8e496a4709a910dd47d940fd68d1fafb1d47
ingridtv <ingridtveten@hotmail.com>
2023-06-27 14:43:25 +0200

    WIP on search bar and single content/two column layout work
    Added some div's + test button

--- learnpathology/views.py
+++ learnpathology/views.py
@@ -12,3 +12,11 @@ def test_one_column(request):
 
 def test_two_column(request):
     return render(request, 'learnpathology/two_column_layout.html')
+
+
+def test_one_column(request):
+    return render(request, 'learnpathology/single_content_layout.html')
+
+
+def test_two_column(request):
+    return render(request, 'learnpathology/two_column_layout.html')
