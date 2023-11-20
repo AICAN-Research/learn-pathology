61fc845c5059162bcdfe1fea1b89921a21591b7a
ingridtv <ingridtveten@hotmail.com>
2023-11-20 12:56:01 +0100

    Made `create_thumbnail` function to avoid duplicate code

--- slide/management/commands/insert_slide_data_into_database.py
+++ slide/management/commands/insert_slide_data_into_database.py
@@ -9,7 +9,7 @@ from django.db import transaction
 from learnpathology.settings import BASE_DIR
 from slide.models import Slide
 from tag.models import Tag
-from slide.views import create_thumbnail
+# from slide.views import create_thumbnail
 
 
 class NoMatchingFileError(Exception):
@@ -77,6 +77,18 @@ def match_file_in_subfolder_to_wsi(file_name, slide_folder):
             continue
 
     raise NoMatchingFileError(f"No WSI file matching {file_name} was found in subfolders")
+
+
+def create_thumbnail(path_to_slide, slide, thumbnails_dir):
+    image_pyramid = fast.WholeSlideImageImporter.create(path_to_slide) \
+        .runAndGetOutputData()
+    access = image_pyramid.getAccess(fast.ACCESS_READ)
+    image = access.getLevelAsImage(level=image_pyramid.getNrOfLevels() - 1)
+    fast_image = fast.Image.createFromArray(np.asarray(image))
+    resized_image = fast.ImageResizer.create(width=512, height=512).connect(fast_image)
+    fast.ImageExporter.create(os.path.join(thumbnails_dir, f'{slide.id}.jpg')) \
+        .connect(resized_image) \
+        .run()
 
 
 # =====================================
@@ -180,19 +192,7 @@ class Command(BaseCommand):
 
                     if path_to_slide.endswith('.vsi'):
                         try:
-                            imagePyramid = fast.WholeSlideImageImporter.create(path_to_slide).runAndGetOutputData()
-
-                            image = imagePyramid.getAccess(fast.ACCESS_READ).getLevelAsImage(
-                                level=imagePyramid.getNrOfLevels() - 1)
-
-                            fast_image = fast.Image.createFromArray(np.asarray(image))
-
-                            resized_image = fast.ImageResizer.create(width=512, height=512).connect(fast_image)
-
-                            exporter = fast.ImageExporter.create(
-                                os.path.join(thumbnails_dir, f'{slide.id}.jpg')).connect(
-                                resized_image).run()
-
+                            create_thumbnail(path_to_slide, slide, thumbnails_dir)
                         except Exception as exc:
                             print(exc)
 
@@ -205,18 +205,7 @@ class Command(BaseCommand):
                 if str(slide.id) + '.jpg' not in os.listdir(thumbnails_dir) \
                         and path_to_slide.endswith('.vsi'):
                     try:
-                        imagePyramid = fast.WholeSlideImageImporter.create(path_to_slide).runAndGetOutputData()
-
-                        image = imagePyramid.getAccess(fast.ACCESS_READ).getLevelAsImage(
-                            level=imagePyramid.getNrOfLevels() - 1)
-
-                        fast_image = fast.Image.createFromArray(np.asarray(image))
-
-                        resized_image = fast.ImageResizer.create(width=512, height=512).connect(fast_image)
-
-                        exporter = fast.ImageExporter.create(os.path.join(thumbnails_dir, f'{slide.id}.jpg')).connect(
-                            resized_image).run()
-
+                        create_thumbnail(path_to_slide, slide, thumbnails_dir)
                     except Exception as exc:
                         print(exc)
 
