eaee88569efbc237b0b6c9993df4392d11a71aef
ingridtv <ingridtveten@hotmail.com>
2023-11-22 11:09:23 +0100

    Update task breadcrumbs and menu highlighting

--- learnpathology/static/learnpathology/learnpathology.js
+++ learnpathology/static/learnpathology/learnpathology.js
@@ -42,6 +42,19 @@ function expandSidebar() {
 
 function highlightMenuItem() {
     let menu_item = '';
+    let app_name = '';
+
+    let url_is_task_do = location.href.includes('multiple_choice/do')
+        || location.href.includes('free_text/do')
+        || location.href.includes('click_question/do')
+        || location.href.includes('one_to_one/do')
+        || location.href.includes('many_to_one/do');
+    let url_is_task_edit = location.href.includes('multiple_choice/edit')
+        || location.href.includes('free_text/edit')
+        || location.href.includes('click_question/edit')
+        || location.href.includes('one_to_one/edit')
+        || location.href.includes('many_to_one/edit');
+
     if (location.href.includes('slide')){
         menu_item = 'slide';
     } else if (location.href.includes('course')) {
@@ -49,6 +62,9 @@ function highlightMenuItem() {
     // } else if (location.href.includes('task/list')) {
     } else if (location.href.includes('task')) {
         menu_item = 'task';
+    } else if (url_is_task_do || url_is_task_edit) {
+        menu_item = '';
+        app_name = 'task';
     } else if (location.href.includes('tag/')) {
         menu_item = 'tag';
     } else {
@@ -56,11 +72,13 @@ function highlightMenuItem() {
     }
     /* TODO: Check menu options that no duplicates/errors/etc. exist */
 
-    console.log('URL:', window.location.href);
-    console.log('Menu item:', menu_item);
-    let domain_index =  window.location.href.indexOf(menu_item);
-    let long_app_name = window.location.href.slice(domain_index);
-    let app_name = long_app_name.slice(0, long_app_name.indexOf('/'));
+    if (!(menu_item === '') && (app_name === '')) {
+        let domain_index =  window.location.href.indexOf(menu_item);
+        let long_app_name = window.location.href.slice(domain_index);
+        app_name = long_app_name.slice(0, long_app_name.indexOf('/'));
+    } else {
+        app_name = 'task';
+    }
     console.log('App name:', app_name);
 
     // First, remove active class from all menu items
