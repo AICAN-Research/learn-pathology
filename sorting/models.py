ba3a47661366e33432d1871838e2e37d155402ec
etiennegaucher <etienne.gaucher@ensiie.fr>
2024-03-05 14:50:16 +0100

    Update max_length of sorting fields

--- sorting/models.py
+++ sorting/models.py
@@ -7,8 +7,8 @@ class SortingTask(models.Model):
     A sorting task
     """
     task = models.OneToOneField(Task, on_delete=models.CASCADE)
-    question = models.CharField(max_length=1000)
-    instructions = models.CharField(max_length=1000,
+    question = models.CharField(max_length=2048)
+    instructions = models.CharField(max_length=2048,
                                     default='Sort the elements on the right to their counterparts on the left.')
 
 
@@ -17,5 +17,5 @@ class Pair(models.Model):
     A pair in a sorting task
     """
     sorting_task = models.ForeignKey(SortingTask, on_delete=models.CASCADE)
-    fixed = models.CharField(max_length=200)
-    draggable = models.CharField(max_length=200)
+    fixed = models.CharField(max_length=512)
+    draggable = models.CharField(max_length=512)
