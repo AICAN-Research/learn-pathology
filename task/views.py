facc7b16df8994ea2e3c01538fda8e49c0973a20
etiennegaucher <etienne.gaucher@ensiie.fr>
2024-10-14 15:15:26 +0200

    Fixed bug when filtering on slides in task creation

--- task/views.py
+++ task/views.py
@@ -85,7 +85,7 @@ def new3(request, course_id=None):
 @teacher_required
 def new2(request, course_id=None):
     last_url = request.META.get('HTTP_REFERER', '')
-    prev_context = {} if ('course/slide_selection/' not in last_url) else request.session.get('image_browser_context',
+    prev_context = {} if ('task/new2/' not in last_url) else request.session.get('image_browser_context',
                                                                                               {})
     request.session['image_browser_context'] = {}
     context = {}
