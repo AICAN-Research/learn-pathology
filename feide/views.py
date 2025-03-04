8a981e11dafc74ff7d4e7e35b6df9df3659eba2e
Erik Smistad <ersmistad@gmail.com>
2025-03-04 14:22:05 +0100

    Update views.py

--- feide/views.py
+++ feide/views.py
@@ -111,7 +111,7 @@ class DataportenAdapter(OAuth2Adapter):
                     for aff in group['membership']['affiliation']:
                         if aff == 'student':
                             student = True
-                if group['id'] == 'fc:fs:fs:prg:ntnu.no:CMED':
+                if group['id'] == 'fc:fs:fs:prg:ntnu.no:CMED' or group['id'].startswith('fc:fs:fs:kull:ntnu.no:CMED'):
                     for aff in group['membership']['fsroles']:
                         if aff == 'STUDENT':
                             medical_student = True
