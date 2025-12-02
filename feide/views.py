be077439498e1d5bdb4cf7f58349100730b02f2c
Erik Smistad <ersmistad@gmail.com>
2025-12-02 08:57:01 +0100

    Updated django-allauth to version 0.63.6

--- feide/views.py
+++ feide/views.py
@@ -1,6 +1,4 @@
 from datetime import datetime
-
-from allauth.socialaccount.adapter import get_adapter
 from allauth.socialaccount.helpers import render_authentication_error, complete_social_login
 from allauth.socialaccount.models import SocialLogin
 from allauth.socialaccount.providers.base import ProviderException, AuthAction, AuthError
@@ -17,140 +15,10 @@ from oauthlib.oauth2 import OAuth2Error
 from requests import RequestException
 
 from .models import ApprovedUser
-from .provider import DataportenProvider
+from .provider import DataportenProvider, DataportenAdapter
 import json
 
-
-class DataportenAdapter(OAuth2Adapter):
-    provider_id = DataportenProvider.id
-    access_token_url = "https://auth.dataporten.no/oauth/token"
-    authorize_url = "https://auth.dataporten.no/oauth/authorization"
-    profile_url = "https://auth.dataporten.no/userinfo"
-    groups_url = "https://groups-api.dataporten.no/groups/me/groups"
-    extended_user_info = 'https://api.dataporten.no/userinfo/v1/userinfo'
-
-    def complete_login(self, request, app, token, **kwargs):
-        """
-        Arguments:
-            request - The get request to the callback URL
-                        /accounts/dataporten/login/callback.
-            app - The corresponding SocialApp model instance
-            token - A token object with access token given in token.token
-        Returns:
-            Should return a dict with user information intended for parsing
-            by the methods of the DataportenProvider view, i.e.
-            extract_uid(), extract_extra_data(), and extract_common_fields()
-        """
-        # The authentication header
-        headers = {"Authorization": "Bearer " + token.token}
-
-        # Userinfo endpoint, for documentation see:
-        # https://docs.dataporten.no/docs/oauth-authentication/
-        userinfo_response = (
-            get_adapter()
-            .get_requests_session()
-            .get(
-                self.profile_url,
-                headers=headers,
-            )
-        )
-        # Raise exception for 4xx and 5xx response codes
-        userinfo_response.raise_for_status()
-
-        # Check that user belongs to NTNU and check whether person is on the approved user list
-        ntnu_found = False
-        approved_user = False
-        for userid_sec in userinfo_response.json()['user']['userid_sec']:
-            username = userid_sec.split(':')[1]
-            org = username.split('@')[1]
-            if org == 'ntnu.no':
-                ntnu_found = True
-                # Check if user is on the pre approved list
-                try:
-                    user = ApprovedUser.objects.get(username=username)
-                    approved_user = True
-                except ApprovedUser.DoesNotExist:
-                    pass
-        if not ntnu_found:
-            raise ProviderException('Only NTNU users are allowed to login to LearnPathology.')
-
-        #print(json.dumps(userinfo_response.json(), indent=2))
-        userinfo2_response = (
-            get_adapter()
-                .get_requests_session()
-                .get(
-                self.extended_user_info,
-                headers=headers,
-            )
-        )
-        #print(json.dumps(userinfo2_response.json(), indent=2))
-
-        # Get group info
-        groupinfo_response = (
-            get_adapter()
-                .get_requests_session()
-                .get(
-                self.groups_url,
-                headers=headers,
-            )
-        )
-
-        # Check if user is enrolled in medical studies
-        #print(json.dumps(groupinfo_response.json(), indent=2))
-        ntnu_found = False
-        medical_student = False
-        student = False
-        faculty = False
-        #enrolled_in_relevant_course = False
-        if not approved_user:
-            for group in groupinfo_response.json():
-                if group['id'] == 'fc:org:ntnu.no':
-                    ntnu_found = True
-                    if group['membership']['primaryAffiliation'] == 'faculty':
-                        faculty = True
-                    for aff in group['membership']['affiliation']:
-                        if aff == 'student':
-                            student = True
-                if group['id'] == 'fc:fs:fs:prg:ntnu.no:CMED' or group['id'].startswith('fc:fs:fs:kull:ntnu.no:CMED') or \
-                        group['id'].startswith('fc:fs:fs:emne:ntnu.no:MDI4043'):
-                    for aff in group['membership']['fsroles']:
-                        if aff == 'STUDENT':
-                            medical_student = True
-                # elif group['id'].find('emne:ntnu.no'):
-                #     course_code = group['id'].split(':')[-2]
-                #     if course_code in ['MD4012', 'MD4020', 'MDT4030', 'MDL4030',
-                #                        'MDA4030', 'MD4042', 'MD4043', 'MD4041',
-                #                        'MD4044', 'MD4045']:
-                #         # Check that within date in group['membership']['notAfter']
-                #         if 'notAfter' in group['membership']:
-                #             notAfterDate = datetime.strptime(group['membership']['notAfter'], '%Y-%m-%dT%H:%M:%SZ')
-                #             print(group['membership']['notAfter'], notAfterDate)
-                #             if notAfterDate < datetime.now():
-                #                 enrolled_in_relevant_course = True
-
-        if not medical_student and not approved_user:
-            raise ProviderException('Only medical students and teachers at NTNU are allowed to login to LearnPathology')
-
-        # The endpoint returns json-data and it needs to be decoded
-        extra_data = userinfo_response.json()["user"]
-
-        # Finally test that the audience property matches the client id
-        # for validification reasons, as instructed by the Dataporten docs
-        # if the userinfo-response is used for authentication
-        if userinfo_response.json()["audience"] != app.client_id:
-            raise ProviderException(
-                "Dataporten returned a user with an audience field \
-                 which does not correspond to the client id of the \
-                 application."
-            )
-
-        return self.get_provider().sociallogin_from_response(
-            request,
-            extra_data,
-        )
-
-
-class FeideCallbackView(OAuth2View):
+class FeideCallbackView(OAuth2CallbackView):
     """
     We override the OAuth2CallbackView to handle errors in authorization.
     """
@@ -167,7 +35,7 @@ class FeideCallbackView(OAuth2View):
                 request, self.adapter.provider_id, error=error
             )
         app = self.adapter.get_provider().app
-        client = self.get_client(self.request, app)
+        client = self.adapter.get_client(self.request, app)
 
         try:
             access_token = self.adapter.get_access_token_data(request, app, client)
