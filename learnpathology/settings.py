c3a1189f973829d74eb277d5d10df8d00b7f5bfc
etiennegaucher <etienne.gaucher@sintef.no>
2026-07-24 11:08:55 +0200

    Fix DATABASE_DIR path

--- learnpathology/settings.py
+++ learnpathology/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = os.environ.get("DATA_DIR", default=BASE_DIR)
 
 # Path where to save the db.sqlite3 file
-DATABASE_DIR = os.environ.get("DATABASE_DIR", default=BASE_DIR)
+DATABASE_DIR = Path(os.environ.get("DATABASE_DIR", default=BASE_DIR))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
